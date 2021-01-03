b29f64e0d5236e9a481de787fec645a3e371a37f
Ivaylo Zlatanov <iizlatanov@gmail.com>
2021-01-03 15:59:09 +0200

    added lists advanced homework

--- PY-Fundamentals/Homework/05. Lists Advanced/05. The Office.py
+++ PY-Fundamentals/Homework/05. Lists Advanced/05. The Office.py
@@ -1,8 +1,28 @@
-def convert_to_int(el):
-    return int(el)
+employees_happiness_list = input().split()
+happiness_improvement_factor = int(input())
 
 
-nums_as_strings = ["1", "2", "3"]
-
-nums = list(map(convert_to_int, nums_as_strings))
-print(nums)
+# employees_happiness_list = input().split()
+# happiness_improvement_factor = int(input())
+# list_after_multiplying = []
+# average_count = 0
+# number_of_employees = 0
+#
+# for employee in employees_happiness_list:
+#     employee_cast_to_int = int(employee)
+#     employee = employee_cast_to_int * happiness_improvement_factor
+#     list_after_multiplying.append(employee)
+#     number_of_employees += 1
+#     average_count += employee
+#
+# average = average_count / number_of_employees
+# happy_employees_count = 0
+#
+# for el in list_after_multiplying:
+#     if el >= average:
+#         happy_employees_count += 1
+#
+# if happy_employees_count < number_of_employees / 2:
+#     print(f"Score: {happy_employees_count}/{number_of_employees}. Employees are not happy!")
+# elif happy_employees_count >= number_of_employees / 2:
+#     print(f"Score: {happy_employees_count}/{number_of_employees}. Employees are happy!")
--- PY-Fundamentals/Homework/05. Lists Advanced/test.py
+++ PY-Fundamentals/Homework/05. Lists Advanced/test.py
@@ -1,6 +1,13 @@
-numbers = input().split(", ")
-result_list = []
+# numbers = input().split(", ")
+# result_list = []
+#
+# for num in numbers:
+#     if num % 2 == 0:
+#         result_list.append(num[index])
 
-for num in numbers:
-    if num % 2 == 0:
-        result_list.append(num[index])
+nums = [1, 2, 3, 4, 5]
+
+elements = []
+
+for index in range(1, 4):
+    pass
