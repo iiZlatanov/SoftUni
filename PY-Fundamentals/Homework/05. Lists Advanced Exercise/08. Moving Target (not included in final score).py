34576de456368d8d78599ff9e6a11be7e9600e06
Ivaylo Zlatanov <iizlatanov@gmail.com>
2021-01-04 16:52:51 +0200

    added lists advanced exercise homework(8)

--- PY-Fundamentals/Homework/05. Lists Advanced Exercise/08. Moving Target (not included in final score).py
+++ PY-Fundamentals/Homework/05. Lists Advanced Exercise/08. Moving Target (not included in final score).py
@@ -1,1 +1,37 @@
-targets_values = int(input()).split()
+numbers = input().split()
+targets_values = [int(x) for x in numbers]
+command_and_values = input().split()
+values_list = [int(x) for x in command_and_values if len(x) <= 2]
+command = command_and_values[0]
+
+while command != "End":
+    if command == "Shoot":
+        index = values_list[0]
+        power = values_list[1]
+        if index <= (len(targets_values) - 1):
+            targets_values[index] -= power
+            if targets_values[index] <= 0:
+                targets_values.pop(index)
+    elif command == "Add":
+        index = values_list[0]
+        value = values_list[1]
+        if index <= (len(targets_values) - 1):
+            targets_values.insert(index, value)
+        else:
+            print("Invalid placement!")
+    elif command == "Strike":
+        index = values_list[0]
+        radius = values_list[1]
+        if (index + radius - 1) <= len(targets_values) and index >= radius:
+            targets_values.pop(index)
+            while radius != 0:
+                targets_values.pop(index)
+                radius -= 1
+        else:
+            print("Strike missed!")
+
+    command_and_values = input().split()
+    values_list = [int(x) for x in command_and_values if len(x) <= 2]
+    command = command_and_values[0]
+
+print("|".join(map(str, targets_values)))
--- PY-Fundamentals/Homework/05. Lists Advanced Exercise/test.py
+++ PY-Fundamentals/Homework/05. Lists Advanced Exercise/test.py
@@ -1,4 +1,2 @@
-shell_number = 3
 
-variable = 2 * shell_number ** 2
-print(variable)
+r = 3
