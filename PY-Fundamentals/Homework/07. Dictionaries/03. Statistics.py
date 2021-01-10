61530eb3084a676ed00ffd368df101e6ec78b97f
Ivaylo Zlatanov <iizlatanov@gmail.com>
2021-01-10 20:25:49 +0200

    added dictionaries homework (3)

--- PY-Fundamentals/Homework/07. Dictionaries/03. Statistics.py
+++ PY-Fundamentals/Homework/07. Dictionaries/03. Statistics.py
@@ -1,12 +1,18 @@
 data = input()
-my_dict = {}
+products_and_quantities = {}
+total_quantity = 0
 
 while data != "statistics":
     data_list = data.split(": ")
-    if data_list[0] in my_dict:
-        my_dict[data_list[0]] += int(data_list[1])
+    if data_list[0] in products_and_quantities:
+        products_and_quantities[data_list[0]] += int(data_list[1])
     else:
-        my_dict[data_list[0]] = int(data_list[1])
+        products_and_quantities[data_list[0]] = int(data_list[1])
     data = input()
 
-print(my_dict)
+for values in products_and_quantities.values():
+    total_quantity += values
+print("Products in stock:")
+for product, quantity in products_and_quantities.items():
+    print(f"- {product}: {quantity}")
+print(f"Total Products: {len(products_and_quantities)}\nTotal Quantity: {total_quantity}")
