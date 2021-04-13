8b9aa4f5ed06947f00db95af039a91d1d6d4b196
Ivaylo Zlatanov <iizlatanov@gmail.com>
2021-04-13 16:46:13 +0300

    Added Multidimensional Lists exercise homework (4)

---
+++ PY-Advanced/Homework/03. Multidimensional Lists Exercise/04. Matrix Shuffling.py
@@ -0,0 +1,24 @@
+rows, columns = list(map(int, input().split()))
+matrix = []
+
+for _ in range(rows):
+    matrix.append(input().split())
+
+command = input().split()
+
+while True:
+    if command[0] != "swap" or len(command) != 5 or int(command[1]) > (rows - 1) or int(command[2]) > (columns - 1) \
+            or int(command[3]) > (rows - 1) or int(command[4]) > (columns - 1):
+        print("Invalid input!")
+    else:
+        a = matrix[int(command[1])][int(command[2])]
+        b = matrix[int(command[3])][int(command[4])]
+        matrix[int(command[1])][int(command[2])] = b
+        matrix[int(command[3])][int(command[4])] = a
+        for each_list in matrix:
+            for element in each_list:
+                print(element, end=" ")
+            print()
+    command = input().split()
+    if command[0] == "END":
+        break
