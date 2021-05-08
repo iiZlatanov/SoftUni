e3283e7ca6cf6000c89bdf7e25398dbe4ec48388
Ivaylo Zlatanov <iizlatanov@gmail.com>
2021-05-08 16:05:27 +0300

    Redo Functions exercise homework (1)

--- PY-Fundamentals/Homework/04. Functions Exercise/01. Smallest of Three Numbers.py
+++ PY-Fundamentals/Homework/04. Functions Exercise/01. Smallest of Three Numbers.py
@@ -1,12 +1,10 @@
-def return_the_smallest_number(x, y, z):
-    just_a_list = []
-    just_a_list.append(x)
-    just_a_list.append(y)
-    just_a_list.append(z)
-    return min(just_a_list)
+first_integer, second_integer, third_integer = int(input()), int(input()), int(input())
 
 
-first_number = int(input())
-second_number = int(input())
-third_number = int(input())
-print(return_the_smallest_number(first_number, second_number, third_number))
+def smallest_given_number(first_number: int or float, second_number: int or float, third_number: int or float):
+    list_with_the_numbers = [first_number, second_number, third_number]
+    result = min(list_with_the_numbers)
+    return result
+
+
+print(smallest_given_number(first_integer, second_integer, third_integer))
