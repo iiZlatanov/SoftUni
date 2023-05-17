986578e6a696f0ecff26a7ebe28a34f115129bfc
Ivaylo Zlatanov <iizlatanov@gmail.com>
2023-05-17 20:29:30 +0300

    Stacks, Queues, Tuples and Sets - Exercise

--- PY-Advanced/PY-Advanced/Homework/02.1. Stacks, Queues, Tuples and Sets Exercise/06. Paint Colors.py
+++ PY-Advanced/PY-Advanced/Homework/02.1. Stacks, Queues, Tuples and Sets Exercise/06. Paint Colors.py
@@ -1,35 +1,60 @@
-# from collections import deque
-# from math import floor
-# data = deque(input().split())
-#
-# main_colors = ["red", "yellow", "blue"]
-# secondary_colors = ["orange", "purple", "green"]
-# result = []
-# waiting_for_second_check = []
-# required_colors = {
-#     "orange": {"red", "yellow"},
-#     "purple": {"red", "blue"},
-#     "green": {"yellow", "blue"}
-# }
-#
-# while data:
-#     first_word = data.popleft()
-#     second_word = data.pop() if data else ""
-#     if first_word + second_word in main_colors:
-#         result.append(first_word + second_word)
-#     elif second_word + first_word in main_colors:
-#         result.append(second_word + first_word)
-#     elif ((first_word + second_word in secondary_colors) or (second_word + first_word in secondary_colors)) and ((required_colors[first_word + second_word] in result) or (required_colors[second_word + first_word] in result):
-#         result.append(first_word + second_word)
-#     elif (first_word + second_word or second_word + first_word) in secondary_colors:
-#         waiting_for_second_check.append(first_word + second_word)
-#     else:
-#         first_word = first_word[:-1]
-#         second_word = second_word[:-1]
-#         data.insert(floor(len(data) / 2), first_word)
-#         data.insert(floor(len(data) / 2), second_word)
-#     for el in waiting_for_second_check:
-#         if required_colors[el] in result:
-#             result.append(el)
-#
-# print(result)
+from copy import copy
+from collections import deque
+
+secondary_colors_waiting_list = []
+result_colors = []
+main_colors = ["red", "blue", "yellow"]
+secondary_colors = ["orange", "purple", "green"]
+strings_data = deque(input().split())
+
+
+def are_secondary_color_conditions_met(check_color):
+    if check_color == "orange":
+        if ("red" in result_colors) and ("yellow" in result_colors):
+            result_colors.append(check_color)
+            secondary_colors_waiting_list.pop(check_color)
+    elif check_color == "purple":
+        if ("red" in result_colors) and ("blue" in result_colors):
+            result_colors.append(check_color)
+            secondary_colors_waiting_list.pop(check_color)
+    elif check_color == "green":
+        if ("yellow" in result_colors) and ("blue" in result_colors):
+            result_colors.append(check_color)
+            secondary_colors_waiting_list.pop(check_color)
+
+
+while True:
+    if len(strings_data) == 1: # Here is when there is only 1 string left to operate with
+        if strings_data[0] in main_colors:
+            result_colors.append(strings_data.pop())
+        elif strings_data[0] in secondary_colors:
+            secondary_colors_waiting_list.append(strings_data.pop())
+        else:
+            trimmed_string = strings_data.pop()[:-1]
+            if trimmed_string:
+                strings_data.append(trimmed_string)
+
+    elif len(strings_data) > 1:# Here is when there are at least 2 strings to operate with
+        first_string = strings_data.popleft()
+        last_string = strings_data.pop()
+        if first_string + last_string in main_colors:
+            result_colors.append(first_string + last_string)
+        elif first_string + last_string in secondary_colors:
+            secondary_colors_waiting_list.append(first_string + last_string)
+        else:
+            trimmed_first_string = first_string[:-1]
+            if trimmed_first_string:
+                strings_data.insert(len(strings_data) // 2, trimmed_first_string)
+            trimmed_last_string = last_string[:-1]
+            if trimmed_last_string:
+                strings_data.insert(len(strings_data) // 2, trimmed_last_string)
+
+    else:                       # Here we have the break condition, which is met when we have no strings left
+        break
+
+    if secondary_colors_waiting_list:
+        for color in copy(secondary_colors_waiting_list):
+            are_secondary_color_conditions_met(color)
+
+
+print(result_colors)
