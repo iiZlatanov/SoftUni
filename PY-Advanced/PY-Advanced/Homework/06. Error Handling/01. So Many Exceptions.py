f351a3700d47007aa6548826719ce4b6e935b173
Ivaylo Zlatanov <iizlatanov@gmail.com>
2023-05-31 22:06:14 +0300

    Error Handling lecture

---
+++ PY-Advanced/PY-Advanced/Homework/06. Error Handling/01. So Many Exceptions.py
@@ -0,0 +1,20 @@
+numbers_list = [int(x) for x in input().split(", ")]
+result = 1
+
+for number in numbers_list:
+    if number <= 5:
+        result *= number
+    elif 5 < number <= 10:
+        result /= number
+
+print(float(result))
+
+
+
+# numbers_list = int(input()).split(", ") result = 1
+#
+# for i in range(numbers_list): number = numbers_list[i+1] if number <= 5
+# result *= number elif 5 < number <= 10:
+# result /= number
+#
+# print(total)
