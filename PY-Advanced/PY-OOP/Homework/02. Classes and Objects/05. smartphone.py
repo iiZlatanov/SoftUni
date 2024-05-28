5b2dfd0deac4bac370a204dec1e7f9011e29e580
Ivaylo Zlatanov <iizlatanov@gmail.com>
2024-05-28 17:28:10 +0300

    Classes and Objects lecture

---
+++ PY-Advanced/PY-OOP/Homework/02. Classes and Objects/05. smartphone.py
@@ -0,0 +1,36 @@
+from typing import List
+
+class Smartphone:
+    def __init__(self, memory: int):
+        self.memory = memory
+        self.apps: List[str] = []
+        self.is_on: bool = False
+
+    def power(self) -> None:
+        self.is_on = not self.is_on
+
+    def install(self, app, app_memory) -> str:
+        if self.memory < app_memory:
+            return f"Not enough memory to install {app}"
+
+        if self.is_on:
+            self.memory -= app_memory
+            self.apps.append(app)
+            return f"Installing {app}"
+
+        return f"Turn on your phone to install {app}"
+
+    def status(self) -> str:
+        return f"Total apps: {len(self.apps)}. Memory left: {self.memory}"
+
+smartphone = Smartphone(100)
+
+print(smartphone.install("Facebook", 60))
+
+smartphone.power()
+
+print(smartphone.install("Facebook", 60))
+print(smartphone.install("Messenger", 20))
+print(smartphone.install("Instagram", 40))
+
+print(smartphone.status())
